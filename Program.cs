5e353ef7f98e200ca872161c8113f4cc82d62fbc
GovardhanSahil <govardhannandimedala.ggktech.com>
2017-05-30 19:28:13 +0530

    Sum of 2 n-digit numbers code

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Console.WriteLine("Enter first number:");
+
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum of " + x + " and " + y + " is " + (x + y));
+
+        }
+    }
+}
